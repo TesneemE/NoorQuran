68bc14634cc1eed3db8ba4fa9cabb9394cd32507
[tes] <tesneeme11@gmail.com>
2024-10-17 01:02:33 -0400

    Initial Commit

---
+++ NoorQuran/NoorQuranApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoorQuranApp.swift
+//  NoorQuran
+//
+//  Created by Tes Essa on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct NoorQuranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
